82efa40c63a33ed29a4565a78185735b5ae2595b
martoast <alexmartos96@gmail.com>
2021-04-27 01:31:44 -0700

    second commit

---
+++ config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL", "http://localhost:1337"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "f760f7ff04f0730ed02d85dbb576b44b"),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7615ae4d-9b1e-453d-9b7d-1044e8d59a2c'
+};
